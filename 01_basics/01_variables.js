c1ff9e29d592a80a1e2222cd747fdf5edcb1a9b9
Sabita Kumari Pandit <sabitapanditb@gmail.com>
2025-07-27 19:52:53 +0000

    discussion on variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=23445
+let accountEmail="sabita@goole.com"
+var accountPassword="2345"
+accountCity="sarsa"
+let accountState;
+
+//accountId=2// not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="1234321"
+accountCity="janakpur"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
