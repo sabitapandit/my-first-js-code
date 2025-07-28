7fa7e42e138aba74914b2dcc8dde76225b4864bd
Sabita Kumari Pandit <sabitapanditb@gmail.com>
2025-07-28 18:26:41 +0000

    datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,32 @@
+"use strict";//treat all js code as newer version//
+
+//alert(3+3)// we are using nodejs, not browser
+
+console.log(3
+   
+    +
+    3)//code readability should be high
+   
+    console.log("sabita")
+
+
+    
+    let name="sabita"
+    let age=20
+    let state;
+   
+   
+   
+    let isloggedIn=false
+    //number => 2 to power 53
+    //bigint
+    //string =>""
+    //boolean => true/false
+    //null => standalone value
+    //undefind =>
+    //symbol =>unique
+    
+    
+    //object
+    console.log("typeof undefined");//undefined
+    console.log("typeof null");//object
